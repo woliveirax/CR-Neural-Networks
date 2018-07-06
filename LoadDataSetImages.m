3e5cf61f9c0862013f9a0b99e7f8918a40f89729
Wallace Oliveira <wallace9655@gmail.com>
2018-07-06 20:26:02 +0100

    Converted id string to number in leaf data

--- LoadDataSetImages.m
+++ LoadDataSetImages.m
@@ -1,13 +1,11 @@
 function [ out ] = LoadDataSetImages(path)
-%Missing a lot of stuff, first get the size of the path, second create cell
-%array yadayada yada
-
     a = dir(fullfile(path,'*.jpg'));
     n = numel(a);
     imageArray = cell(n,2);
     
     for i = 1:1:n
         [trash imageArray{i,1} trash] = fileparts(a(i).name);
+        imageArray{i,1} = str2num(imageArray{i,1});
         imageArray{i,2} = imread(fullfile(path,a(i).name));
     end
     
