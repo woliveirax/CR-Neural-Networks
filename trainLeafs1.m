164c43dbd94ac5e0e7181523f656009215dfc49d
Joana <jbmaria.joana@gmail.com>
2018-07-07 05:07:25 +0100

    gg 1st train completed

--- trainLeafs1.m
+++ trainLeafs1.m
@@ -1,7 +1,23 @@
 [target input] = PrepareTargetForTraining();
 
-net = feedforwardnet();
+% test = zeros(99, 1000);
+% test = test';
 
-[net,tr] = train(net, input', target);
+net = feedforwardnet();
+input = table2array(input);
+input = double(input);
+input = input';
+
+net.trainFcn = 'traingdx';
+
+[net,tr] = train(net, input, target);
+
 view(net);
 disp(tr)
+
+out = sim(net, input);
+
+axis off;
+plotconfusion(target, out); % Matriz de confusao
+
+plotperf(tr);         % Grafico com o desempenho da rede nos 3 conjuntos
