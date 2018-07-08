ed48754783237b9ee305d9f858b7f797d66c5011
Joana <jbmaria.joana@gmail.com>
2018-07-08 03:13:51 +0100

    target preparation more generic

--- PrepareTargetForTraining.m
+++ PrepareTargetForTraining.m
@@ -1,39 +1,68 @@
-function [target,input] = PrepareTargetForTraining()
-%MAKETARGET Summary of this function goes here
-%   Detailed explanation goes here
-% function PrepareTargetForTraining()
+function [target input] = PrepareTargetForTraining(imgs_path, excell_path, bool_subspecie)     % bool_dataset caso tenhamos tempo
+
+%%Obter info
+%if(!bool_datset) %verificar s info encontra-se em dataset
+dataImages = LoadDataSetImages(imgs_path);
+dataClassifications = ReadLeafExcelData(excell_path);
+
+input = dataImages(:,2);
+
+%%Para não realocar memória em cada ciclo:
+num = numel(input);
+target = zeros(num,num);%alocar previamente memória, para máximo possível de targets resultantes, com posterior eliminação de linhas vazias
+
+tab_species = cell(1,numel(input)); %criar previamente memória para máximo possível de especies
+tab_species{1,numel(input)}=[];
+
+%%Função:
+%species_count = 1;
+
+for i = 1:numel(input)
+    id = dataImages{i,1};
+    specie_found = 0;
+    specie = [];
     
-    dataImages = LoadDataSetImages('.\Resources\folha_1');
-    dataClassifications = ReadLeafExcelData('.\Resources\leaf.xlsx');
-    
-    input = dataImages(:,2);
-    
-    for i = 1:numel(input)
-        col = zeros(numel(input),1);
-        id = dataImages{i,1};
+    for j = 1:numel(dataClassifications(:,1)) %obtem especie a partir de id
         
-        for j = 1:numel(dataClassifications)
-            in = dataClassifications{j,1};
-            
-            if( in == id )
+        in = dataClassifications{j,1};
+        
+        if( in == id )
+            if(bool_subspecie)
+                specie = dataClassifications(j,3);
+            else
                 specie = dataClassifications(j,2);
-                break;
             end
+            break;
         end
-        
-        for j = 1:numel(dataClassifications)
-            leaf_count = 1;
-            if(strcmpi(dataClassifications{j,2},specie))
-                col(i,1)= 1;
-                break;
-            end
-            leaf_count = leaf_count + 1; 
-        end
-  
-        target(:,i) = col(:,1);
         
     end
-
+    
+    if(isempty(specie))
+        continue;
+    end
+    
+    if(i==1)
+        species_count = 1;
+        tab_species(1,species_count) =  specie; %adiciona 1 especie para comparacao.
+        target(1,1) = 1;
+        continue;
+    end
+    
+    for k = 1 : species_count % procuro         
+        if(strcmpi(tab_species{1,k},specie{1,1}))
+            target(k,i) = 1;
+            specie_found = 1;
+            break;
+        end
+    end
+    
+    if(specie_found == 0)
+        species_count = species_count + 1;
+        tab_species(1,species_count) = specie;
+        target(species_count,i) = 1;
+    end
     
 end
 
+target( all(~target,2),:)=[]; % remove linhas com tudo a zero(vazias)
+end
