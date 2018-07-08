2f2e6a210da62dbeef1cb52fd57ade295a06b558
Joana <jbmaria.joana@gmail.com>
2018-07-08 22:13:19 +0100

    criacao de imagem binaria

--- LoadDataSetImages.m
+++ LoadDataSetImages.m
@@ -1,4 +1,4 @@
-function [ out ] = LoadDataSetImages(path)
+function [ out ] = LoadDataSetImages(path,optimization)
     a = dir(fullfile(path,'*.jpg'));
     n = numel(a);
     imageArray = cell(n,2);
@@ -8,6 +8,24 @@ function [ out ] = LoadDataSetImages(path)
         imageArray{i,1} = str2num(imageArray{i,1});
         
         image = imread(fullfile(path,a(i).name));
+
+        switch optimization
+            %converte para vector binario
+            case 1
+                 image = (image >= 255); %%devolve (true -> 1 ou false -> 0)
+                 
+             %deteção de margens a partir de multiplicação dos pixeis pela máscara de Sobel
+            case 2
+%                 sobel_v = [-1, -2, -1; 0, 0, 0; 1, 2, 1];
+%                 sobel_h = [-1, 0, 1; -2, 0, 2; -1, 0, 1];
+%                 
+%                 borda_v = conv2(double(image),sobel_v);
+%                 borda_h = conv2(image,sobel_h);
+%                 borda_v = borda_v(2:end-1,2:end-1);
+%                 borda_h = borda_h(2:end-1,2:end-1);
+%                 image = borda_v + borda_h;
+        end        
+        
         image = imresize(image,[500 500]);
         image = image(:)';
         
